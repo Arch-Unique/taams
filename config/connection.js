6ce7beba124d980f038bbec3379bcaca94905d33
Arch-Unique <ikennaidigo@gmail.com>
2023-01-15 12:06:41 +0100

    fix sql issue

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@ const Sequelize = require("sequelize");
 // module.exports = new Sequelize("montanab_api", "montanab_admin", "mezeokec51", {
 //   dialect: "mysql",
 // });
-module.exports = new Sequelize("unnattendance", "root", "", {
+module.exports = new Sequelize("unnattendance", "root", "montanahomz", {
   dialect: "mysql",
 });
